99a4a734ed4f6498ac8e517483c8fb991ce2ba56
alvesmaicon <alvesmaiconn@gmail.com>
2017-09-06 02:36:42 -0300

    Iniciada tarefa T1, desenvolvimento de um exemplo de biblioteca que possui funcionalidades do OpenCV, construida e instalada com Cmake

---
+++ include/iplib.hpp
@@ -0,0 +1,7 @@
+#ifndef IPLIB_HPP
+#define IPLIB_HPP
+
+void view_cam()
+
+#endif
+
---
+++ sample/sample.cpp
@@ -0,0 +1,12 @@
+#include "iplib/iplib.hpp"
+#include <iostream>
+
+using namespace ip_;
+
+int main(){
+
+	view_cam();
+	
+	return 0;
+
+}
